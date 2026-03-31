c46ba09961f0737ce54000ddc91dfd88cc86f8d4
Illia Kornyk <krekerbrow@gmail.com>
2026-03-31 13:01:45 +0300

    fix: stripe api version

--- full_stack_app/lib/stripe.ts
+++ full_stack_app/lib/stripe.ts
@@ -5,5 +5,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-02-24.acacia',
+  apiVersion: (process.env.STRIPE_API_VERSION || '2026-03-25.dahlia') as any,
 })
